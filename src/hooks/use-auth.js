6737e6517c28e34529d779558c3d1aaf68e66000
Jono Matusky <jono.matusky@gmail.com>
2022-04-20 14:43:37 -0400

    Fix logout bug

--- src/hooks/use-auth.js
+++ src/hooks/use-auth.js
@@ -2,9 +2,11 @@ import { useState, useEffect, useCallback } from 'react'
 import firebase from 'config/firebase'
 import posthog from 'posthog-js'
 import useUserStore from 'hooks/store/use-user-store'
+import useExperienceStore from 'hooks/store/use-experience-store'
 
 export const useAuth = () => {
   const { clearUser } = useUserStore()
+  const { clearExperiences } = useExperienceStore()
 
   const [state, setState] = useState(() => {
     const user = firebase.auth().currentUser
@@ -18,9 +20,10 @@ export const useAuth = () => {
     try {
       await firebase.auth().signOut()
       clearUser()
+      clearExperiences()
       posthog.reset()
     } catch (err) {}
-  }, [clearUser])
+  }, [clearUser, clearExperiences])
 
   useEffect(() => {
     // listen for auth state changes
